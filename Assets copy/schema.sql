942cb40cb23d34727251e67fc060220de9bbeefc
Garrett Dobson <garrettdobson33@gmail.com>
2020-09-17 19:52:52 -0400

    all view functions are working

--- Assets copy/schema.sql
+++ Assets copy/schema.sql
@@ -1,25 +1,61 @@
 DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE database employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
+
 CREATE TABLE department (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
+ 
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
+  manager_id INT,
   role_id INT,
-  manager_id INT
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
 );
 
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+INSERT INTO employee
+    (first_name, last_name, manager_id, role_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
--- tracker.js
+++ tracker.js
@@ -12,7 +12,10 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  startPrompt();
+});
 
+function startPrompt() {
   inquirer
     .prompt({
       name: "action",
@@ -64,36 +67,117 @@ connection.connect(function(err) {
         break;
       }
     });
-});
+}
 
 // will need some inquirer prompts
 
 // Add departments, roles, employees
 
 function addDept() {
-  
+  inquirer
+    .prompt({
+      name: "departmentName",
+      type: "input",
+      message: "What department do you want to add?"
+    })
+    .then(function(answer) {
+      
+      let query = connection.query("INSERT INTO department SET ? ", 
+        {
+          name: answer.name
+        },
+        function(err) {
+          if (err) throw err;
+          console.table(answer);
+          startPrompt();
+        });
+    });
 };
 
 function addRoles() {
+  connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, answer) {
+    inquirer.prompt([
+        {
+          name: "Title",
+          type: "input",
+          message: "What is the roles Title?"
+        },
+        {
+          name: "Salary",
+          type: "input",
+          message: "What is the Salary?"
 
+        } 
+    ]).then(function(answer) {
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: answer.Title,
+              salary: answer.Salary,
+            },
+            function(err) {
+                if (err) throw err
+                console.table(answer);
+                startPrompt();
+            }
+        )
+
+    });
+  });
 };
 
 function addEmployees() {
-
+  inquirer
+    .prompt({
+      name: "employeeName",
+      type: "input",
+      message: "What employee do you want to add?"
+    })
+    .then(function(answer) {
+      const userInput = answer.employeeName
+      let query = "INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?)";
+      connection.query(query, userInput, function(err, res) {
+        if (err) throw err;
+        //viewEmployees();
+      });
+    });
 };
 
 //View departments, roles, employees
+// won't have inquirer 
+//look at LEFT JOINs
 
+//not working
 function viewDept() {
-
+  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+  
+  function(err, answer) {
+    if (err) throw err;
+    console.table(answer);
+    startPrompt();
+  })
 };
 
+
 function viewRoles() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+  
+  function(err, answer) {
+  if (err) throw err;
+  console.table(answer);
+  startPrompt();
+  })
 
 };
 
 function viewEmployees() {
-
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    
+    function(err, answer) {
+      if (err) throw err;
+      console.table(answer);
+      startPrompt();
+  });
 };
 //Update employee roles
 
